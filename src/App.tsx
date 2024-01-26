83c337f79ba73ab15d0d190fa6fa9a0c50e5ae44
unknown <calebjanhunen@gmail.com>
2024-01-26 00:08:59 -0500

    Remove files and add base sass files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.css';
+import Home from './pages/home/home';
 
 export default function App() {
-  return <div className='App'></div>;
+  return <Home />;
 }
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.scss';
+
+export default function Home() {
+  return (
+    <div className='container'>
+      <div className='quiz-container'>
+        <h1>Quiz App</h1>
+      </div>
+    </div>
+  );
+}
